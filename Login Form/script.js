6662a39a93eb9139672faeae459385d8809f75bd
Fxanz <107518652+Fxanz@users.noreply.github.com>
2023-05-20 09:41:42 +0800

    reset

--- Login Form/script.js
+++
@@ -1,9 +0,0 @@
-function login() {
-  var user = document.getElementById("username").value;
-  var pass = document.getElementById("password").value;
-  if (user == "admin" && pass == "123") {
-    alert("Hello Admin");
-  } else {
-    alert("You are not allowed to enter!");
-  }
-}
